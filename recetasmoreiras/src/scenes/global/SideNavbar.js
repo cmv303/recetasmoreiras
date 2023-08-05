e9b03626ddb9ea2e2534e44ad9c79b2939a58f38
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-08-05 16:45:16 -0400

    feat: add sidebar to lefthand side
    functionality is still to come, but using placeholders to get it showing

--- recetasmoreiras/src/scenes/global/SideNavbar.js
+++ recetasmoreiras/src/scenes/global/SideNavbar.js
@@ -1,151 +1,60 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import { tokens } from '../../theme';
-import { Box, Icon, IconButton, Typography, useTheme } from '@mui/material';
-import HomeOutlined from '@mui/icons-material/HomeOutlined';
-import OutdoorGrillOutlined from '@mui/icons-material/OutdoorGrillOutlined';
-import PersonOutlined from '@mui/icons-material/PersonOutlined';
-import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-
-const Item = ({ title, to, icon, selected, setSelected }) => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  return (
-    <MenuItem
-      active={selected === title}
-      style={{ color: colors.grey[100] }}
-      onClick={() => setSelected(title)}
-      icon={icon}
-    >
-      <Typography>{title}</Typography>
-      <Link to={to} />
-    </MenuItem>
-  );
-};
+import {
+  Box,
+  IconButton,
+  useTheme,
+  InputBase,
+  Icon,
+  Divider,
+  List,
+  ListItemIcon,
+} from '@mui/material';
+import { useContext } from 'react';
+import { ColorModeContext, tokens } from '../../theme';
+import OutdoorGrillOutLinedIcon from '@mui/icons-material/OutdoorGrillOutlined';
+import AdminOutLinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 
 function SideNavbar() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const [isCollapsed, setIsCollapsed] = useState(false);
-  const [selected, setSelected] = useState('Dashboard');
+  const colorMode = useContext(ColorModeContext);
   return (
-    // SIDENAVBAR styling to override pro-sidebar defaults
     <Box
-      sx={{
-        '& .pro-sidebar-inner': {
-          background: `${colors.primary[400]} !important`,
-        },
-        '& .pro-icon-wrapper': {
-          backgroundColor: 'transparent !important',
-        },
-        '& .pro-inner-item': {
-          padding: '5px 35px 5px 20px !important',
-        },
-        '& .pro-inner-item:hover': {
-          color: '#868dfb !important',
-        },
-        '& .pro-menu-item.active': {
-          color: '#6870fa !important',
-        },
-      }}
+      display="flex"
+      justifyContent="center"
+      flexDirection={'column'}
+      alignItems={'center'}
+      sx={{ width: 240, height: '100vh', background: '#3e4396' }}
     >
-      <Sidebar collapsed={isCollapsed}>
-        <Menu iconShape="square">
-          {/* LOGO AND MENU ICON */}
+      <List>
+        <ListItemIcon>
+          <ListOutlinedIcon />
+          This is Hamburger icon just as a menu placeholder
+        </ListItemIcon>
+      </List>
+      <Divider />
 
-          {/* WHEN NOT COLLAPSED */}
-          <MenuItem
-            onClick={() => setIsCollapsed(!isCollapsed)}
-            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
-            style={{ margin: '10px 0 20px 0', color: colors.grey[100] }}
-          >
-            {/* SHOWS HAMBURGER ICON AND TYPOGRAPHY WHEN IS COLLAPSED */}
-            {!isCollapsed && (
-              <Box
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                marginLeft="15px"
-              >
-                <Typography variant="h3" color={colors.grey[100]}>
-                  RECETAS MOREIRAS
-                </Typography>
-                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
-                  <MenuOutlinedIcon />
-                </IconButton>
-              </Box>
-            )}
-          </MenuItem>
-        </Menu>
+      <List>
+        <ListItemIcon>
+          <AdminOutLinedIcon />
+          This is one Admin icon
+        </ListItemIcon>
+      </List>
 
-        {/* USER */}
-        {!isCollapsed && (
-          <Box marginBottom="25px">
-            <Box display="flex" justifyContent="center" alignItems="center">
-              <img
-                alt="profile-user"
-                width="100px"
-                height="100px"
-                src={'../public/logo192.png'}
-                style={{ cursor: 'pointer', borderRadius: '50%' }}
-              />
-            </Box>
+      <List>
+        <ListItemIcon>
+          <OutdoorGrillOutLinedIcon />
+          This is an icon representing Recipes
+        </ListItemIcon>
+      </List>
 
-            <Box textAlign="center">
-              <Typography
-                variant="h2"
-                color={colors.grey[100]}
-                fontWeight="bold"
-                sx={{ margin: '10px 0 0 0' }}
-              >
-                Camila Moreiras
-              </Typography>
-              <Typography variant="h5" color={colors.greenAccent[500]}>
-                Software Engineer
-              </Typography>
-            </Box>
-          </Box>
-        )}
-        {/* MENU ITEMS */}
-        <Box paddingLeft={isCollapsed ? undefined : '10%'}>
-          <Item
-            title="Dashboard"
-            to="/"
-            icon={<HomeOutlined />}
-            selected={selected}
-            setSelected={setSelected}
-          />
-          <Typography
-            variant="h6"
-            color={colors.grey[300]}
-            sx={{ margin: '15px 0 5px 20px' }}
-          >
-            Recipe Box
-          </Typography>
-          <Item
-            title="Manage Recipes"
-            to="/recipes"
-            icon={<OutdoorGrillOutlined />}
-            selected={selected}
-            setSelected={setSelected}
-          />
-          <Typography
-            variant="h6"
-            color={colors.grey[300]}
-            sx={{ margin: '15px 0 5px 20px' }}
-          >
-            Admin
-          </Typography>
-          <Item
-            title="Admin"
-            to="/admin"
-            icon={<PersonOutlined />}
-            selected={selected}
-            setSelected={setSelected}
-          />
-        </Box>
-      </Sidebar>
+      <Divider />
+      <List>
+        <ListItemIcon>
+          <AdminOutLinedIcon />
+          This is another Admin icon just as a placeholder
+        </ListItemIcon>
+      </List>
     </Box>
   );
 }
